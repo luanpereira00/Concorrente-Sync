2ba1e1d2fa47880b937b07aa30af212d8c21391d
Joaliton Luan Pereira Ferreira <luanpereira@imd.local>
2018-05-17 16:02:49 -0300

    Starting eclipse project

---
+++ src/unisex_toilet_problem/Main.java
@@ -0,0 +1,10 @@
+package unisex_toilet_problem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello toilet");
+	}
+
+}
