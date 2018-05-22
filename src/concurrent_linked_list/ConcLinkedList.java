552cccee58e49e7f073b94ccb02dec63f8ec1975
Joaliton Luan Pereira Ferreira <luanpereira@imd.local>
2018-05-22 16:25:20 -0300

    Starting linked list branch

---
+++ src/concurrent_linked_list/ConcLinkedList.java
@@ -0,0 +1,24 @@
+package concurrent_linked_list;
+
+import java.util.LinkedList;
+
+public class ConcLinkedList<E> {
+	private LinkedList<E> linkedList;
+	//TODO concorrencia usando alguma ferramenta
+	
+	public ConcLinkedList() {
+		linkedList = new LinkedList<E>();
+	}
+	
+	public void insert() {
+		
+	}
+	
+	public void remove() {
+		
+	}
+	
+	public boolean search(E element) {
+		return false;
+	}
+}
