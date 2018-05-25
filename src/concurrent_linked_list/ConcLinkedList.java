cf4a99024c3b39bdbe46713d4090d1beb61693e0
Joaliton Luan Pereira Ferreira <luanpereira@imd.local>
2018-05-25 09:43:14 -0300

    Generating javadoc

--- src/concurrent_linked_list/ConcLinkedList.java
+++ src/concurrent_linked_list/ConcLinkedList.java
@@ -6,42 +6,47 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * @author luanpereira
+ * A class that defines concurrent linked list
+ * @param <E> Type of list's element
+ */
 public class ConcLinkedList<E> {
-	private Lock lockRemove;
+	private Lock lockRemove; 
 	private Lock lockInsert;
 	private Condition remove;
-	private Condition insert;
 	private boolean isRemoving = false;
-
-	
 	private LinkedList<E> linkedList;
-	//TODO concorrencia usando alguma ferramenta
-	
+		
+	/**
+	 * Constructor for concurrent linked list
+	 */
 	public ConcLinkedList() {
 		linkedList = new LinkedList<E>();
 		lockRemove = new ReentrantLock();
 		lockInsert = new ReentrantLock();
 		remove = lockRemove.newCondition();
-		insert = lockInsert.newCondition();
 	}
 	
+	/**
+	 * Method to insert an element on list
+	 * @param element the element to be inserted
+	 */
 	public void insert(E element) {
+		/**
+		 * @author luanpereira
+		 * Anonymous class for a thread
+		 */
 		(new Thread() {
 			public void run() {
-				
 				lockInsert.lock();
-				System.out.println("------ starting to insert");
+				System.out.println("--- starting to insert");
 				try {
-					try {
-						Thread.sleep(200);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					try_sleep(200);
 					linkedList.add(element);
 					System.out.println(element + " inserted succesfully");
 				} finally {
-					System.out.println("------ finishing to insert");
+					System.out.println("--- finishing to insert");
 					lockInsert.unlock();
 				}
 				
@@ -49,30 +54,30 @@ public class ConcLinkedList<E> {
 		}).start();
 	}
 	
+	/**
+	 * Method to remove an element on list
+	 * @param element element to be removed on list
+	 */
 	public void remove(E element) {
-		//TODO try catch to remove
+		/**
+		 * @author luanpereira
+		 * Anonymous class for a thread
+		 */
 		(new Thread() {
 			public void run() {
-				
 				synchronized (remove) {
 					lockRemove.lock();
 					isRemoving=true;
 					lockInsert.lock();
-					System.out.println("------ starting to remove");
+					System.out.println("----- starting to remove");
 					try {
-						try {
-							Thread.sleep(500);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+						try_sleep(500);
 						linkedList.remove(element);
 						System.out.println(element + " removed succesfully");
 					} finally {
 						isRemoving=false;
 						remove.signalAll();
-						//remove.notifyAll();
-						System.out.println("------ finishing to remove");
+						System.out.println("----- finishing to remove");
 						lockInsert.unlock();
 						lockRemove.unlock();
 						
@@ -83,27 +88,29 @@ public class ConcLinkedList<E> {
 		}).start();
 	}
 	
+	/**
+	 * Method to search an element on list
+	 * @param element element to be searched
+	 * @return true if element is on list, false if not
+	 */
 	public boolean search(E element) {
 		final AtomicBoolean result = new AtomicBoolean();
+		/**
+		 * @author luanpereira
+		 * Anonymous class for a thread
+		 */
 		(new Thread() {
 			public void run() {
 				
 				synchronized (remove) {
 					if(isRemoving) {
-						//remove.wait();
 						remove.awaitUninterruptibly();
 					}
-					System.out.println("------ starting to search");
+					System.out.println("- starting to search");
 					result.set(linkedList.contains(element));
-					try {
-						Thread.sleep(700);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println("------ finishing to search");
+					try_sleep(700);
+					System.out.println("- finishing to search");
 				}
-				
 			}
 		}).start();
 		if(result.get()) {
@@ -113,4 +120,18 @@ public class ConcLinkedList<E> {
 		}
 		return result.get();
 	}
+	
+	/**
+	 * Method only to try apply sleep on others methods
+	 * @param time time to sleep
+	 */
+	void try_sleep(int time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
--- src/concurrent_linked_list/Main.java
+++ src/concurrent_linked_list/Main.java
@@ -1,39 +1,32 @@
 package concurrent_linked_list;
 
+//Questionamente: durante iniciar uma remoção, é necessário esperar as buscas concluirem?
+/**
+ * @author luanpereira
+ * Main class
+ */
 public class Main {
 
+	/**
+	 * Main method
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello linked list");
-		ConcLinkedList c = new ConcLinkedList();
-		c.search("O");
-		c.insert("A");
-		c.search("A");
-		c.insert("B");
-		c.search("A");
-		c.remove("B");
-		c.search("C");
-		c.search("A");
-		c.remove("B");
-		c.insert("C");
-		c.search("A");
-		c.insert("D");
-		c.search("D");
-		c.search("O");
-		c.insert("A");
-		c.search("A");
-		c.insert("B");
-		c.search("A");
-		c.remove("B");
-		c.search("C");
-		c.search("A");
-		c.remove("B");
-		c.insert("C");
-		c.search("A");
-		c.insert("D");
-		c.search("D");
+		ConcLinkedList<Integer> c = new ConcLinkedList<>();
 		
-		//System.out.println(c.top());
+		for(int i=0;i<20; i++) {
+			c.insert(i);
+		}
+		
+		for(int i=0;i<20; i+=2) {
+			c.search(i);
+		}
+		
+		for(int i=0;i<20; i+=3) {
+			c.remove(i);
+		}
 	}
 
 }
